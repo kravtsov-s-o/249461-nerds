b78a624e90d388defdb2b1e950a3b672d55580c3
Sergey <s.kravtsov@hotmail.com>
2016-11-16 12:13:29 +0200

    доработка по js (localStorage)

--- js/write-us.js
+++ js/write-us.js
@@ -1,13 +1,24 @@
 		var link = document.querySelector(".write");
+
 		var popup = document.querySelector(".form-write-us");
 		var close = document.querySelector(".modal-content-close");
-		var login = popup.querySelector("[name=name]");
+
 		var form = popup.querySelector("form");
+		var login = popup.querySelector("[name=name]");
+		var email = popup.querySelector("[name=email]");
+
+		var storage = localStorage.getItem("login");
+		
 
 			link.addEventListener("click", function(event) {
 				event.preventDefault();
 				popup.classList.add("form-write-us-show");
-				login.focus();
+				if (storage) {
+		        	login.value = storage;
+					email.focus();
+				} else {
+					login.focus();
+				}
 			});
 
 			close.addEventListener("click", function(event) {
@@ -19,6 +30,15 @@
 				event.preventDefault();
 			});
 
+
+			form.addEventListener("submit", function(event) {
+		        if (!login.value) {
+		          event.preventDefault();
+		        } else {
+		          localStorage.setItem("login", login.value);
+		        }
+		      });
+
 			window.addEventListener("keydown", function(event) {
 				if (event.keyCode === 27) {
 					if (popup.classList.contains("form-write-us-show")) {
@@ -26,3 +46,4 @@
 					}
 				}
 			});
+
